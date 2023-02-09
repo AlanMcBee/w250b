b7571e66d7828f796182ce24d6337af25b419e52
Alan McBee <alan.mcbee@microsoft.com>
2023-02-09 07:55:23 -0800

    parameterize startup

--- startDeploy.ps1
+++ startDeploy.ps1
@@ -8,30 +8,41 @@
 param (
     [Parameter()]
     [string]
-    $ResourceGroupName
+    $ResourceGroupName,
+
+    # Parameter help description
+    [Parameter()]
+    [string]
+    $ResourceLocation = 'westus2'
+    # Options:
+    #   centralus
+    #   eastus
+    #   eastus2
+    #   northcentralus
+    #   southcentralus
+    #   westcentralus
+    #   westus
+    #   westus2
+    #   westus3
 )
 
 Set-StrictMode -Version Latest 
 
-[string] $rgName = Read-Host -Prompt 'Resource Group Name (see https://learn.microsoft.com/azure/azure-resource-manager/management/resource-name-rules#microsoftresources)'
-[string] $resourceLocation = 'westus2'
-# Options:
-#   centralus
-#   eastus
-#   eastus2
-#   northcentralus
-#   southcentralus
-#   westcentralus
-#   westus
-#   westus2
-#   westus3
+if ($PSBoundParameters.ContainsKey('ResourceGroupName'))
+{
+    $rgName = $ResourceGroupName
+}
+else
+{
+    $rgName = Read-Host -Prompt 'Resource Group Name (see https://learn.microsoft.com/azure/azure-resource-manager/management/resource-name-rules#microsoftresources)'
+}
 [securestring] $mySqlAdminPassword = Read-Host -Prompt 'MySQL Administrator Password' -AsSecureString
 [securestring] $redcapCommunityPassword = Read-Host -Prompt 'REDCap Community Password' -AsSecureString
 [securestring] $smtpPassword = Read-Host -Prompt 'SMTP Password' -AsSecureString
 
 $deployArgs = @{
     ResourceGroupName                           = $rgName
-    Arm_ResourceLocation                        = $resourceLocation
+    Arm_ResourceLocation                        = $ResourceLocation
     DatabaseForMySql_AdministratorLoginPassword = $mySqlAdminPassword
     ProjectRedcap_CommunityPassword             = $redcapCommunityPassword
     Smtp_UserPassword                           = $smtpPassword
